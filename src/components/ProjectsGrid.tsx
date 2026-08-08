80e07b07794c418408542d0107a7adf31dae16a4
Venkat Vatshal <venkatvatshal@gmail.com>
2026-08-08 16:42:50 +0530

    Organized Cards for Projects

--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -5,19 +5,17 @@ import ProjectCard from "./ProjectCard";
 
 export default function ProjectsGrid() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+    <div className="grid grid-cols-2 gap-lg mt-md">
       {PROJECTS_DATA.map((project, idx) => (
         <ProjectCard
           key={project.id}
           index={idx}
           title={project.title}
           description={project.shortDescription}
-          features={project.features}
           techStack={project.techStack}
           liveUrl={project.liveUrl}
           githubUrl={project.githubUrl}
           href={`/projects/${project.slug}`}
-          accentColor={project.accentColor}
           category={project.category}
         />
       ))}
